98568c37df2d17f363875079380450fe6e033e09
Balazs Nadasdi <yitsushi@gmail.com>
2012-09-19 17:56:37 +0200

    add light favicon fix mongodb local fail

--- env.js
+++ env.js
@@ -1,3 +1,8 @@
+if (typeof process.env.MONGOHQ_URL == 'undefined') {
+  process.env.MONGOHQ_URL = "mongodb://localhost/folyam-engine";
+}
+
+
 module.exports.applicationName = 'Folyam'
 module.exports.mongo = {
   url: process.env.MONGOHQ_URL
